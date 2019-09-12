21a8a6c7c3c0b3d32fb2b8000cce90b18391b580
mkrishnan-codes <manu.krishnan@tarams.com>
2019-09-12 11:33:35 +0530

    Updates

---
+++ precache-manifest.99ac95907693d52f386699659dbaee9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0bb46573ebbf1b068d861b3c6ae250a",
+    "url": "/react-page/index.html"
+  },
+  {
+    "revision": "e5de64e92843819da4a7",
+    "url": "/react-page/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6646c0ff33fc6204c2a0",
+    "url": "/react-page/static/js/2.fdf1ccff.chunk.js"
+  },
+  {
+    "revision": "e5de64e92843819da4a7",
+    "url": "/react-page/static/js/main.bee38ece.chunk.js"
+  },
+  {
+    "revision": "5c0c63eb263d0d4306db",
+    "url": "/react-page/static/js/runtime~main.5e838ec2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
