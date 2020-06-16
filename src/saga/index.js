3fab5edbf8dc6d4ac641bb743dcfe9263dc4b81e
mkrishnan-codes <manu.krishnan@tarams.com>
2020-06-16 12:48:53 +0530

    worker messaging implemented

--- src/saga/index.js
+++ src/saga/index.js
@@ -1,13 +1,13 @@
 import { fork, all } from 'redux-saga/effects';
 import authSaga from './auth-saga';
 import itemsSaga from './items-saga';
-import { watchActionMessage, watchWorkerMessages } from './search-worker-saga';
+import { workerRecieverSaga, workerSenderSaga } from './search-worker-saga';
 
 export default function* rootSaga() {
 	yield all([
 		fork(authSaga),
 		fork(itemsSaga),
-		fork(watchActionMessage),
-		fork(watchWorkerMessages),
+		fork(workerRecieverSaga),
+		fork(workerSenderSaga),
 	]);
 }
